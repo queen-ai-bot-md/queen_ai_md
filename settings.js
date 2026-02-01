5ee20802624c4e91623e790bf5817e4c5eb2cddf
queen-ai-bot-md <queenaibotmd@gmail.com>
2026-02-01 17:09:26 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '༺『Q』『U』『E』『E』『N』 ❀『A』『i』༻/n/n/n/n/n/n/n/n𝓑𝓵𝓪𝓬𝓴 𝓴𝓲𝓷𝓰 𝓵𝓮𝓸𝓷𝓲𝓭𝓪𝓼 (𝓣𝓱𝓮 𝓷𝓮𝓶𝓮𝓼𝓲𝓼 𝓹𝓻𝓲𝓶𝓮',
+  author: '𝓑𝓵𝓪𝓬𝓴 𝓴𝓲𝓷𝓰 𝓵𝓮𝓸𝓷𝓲𝓭𝓪𝓼 (𝓣𝓱𝓮 𝓷𝓮𝓶𝓮𝓼𝓲𝓼 𝓹𝓻𝓲𝓶𝓮',
+  botName: "༺『Q』『U』『E』『E』『N』 ❀『A』『i』༻",
+  botOwner: '𝓑𝓵𝓪𝓬𝓴 𝓴𝓲𝓷𝓰 𝓵𝓮𝓸𝓷𝓲𝓭𝓪𝓼 (𝓣𝓱𝓮 𝓷𝓮𝓶𝓮𝓼𝓲𝓼 𝓹𝓻𝓲𝓶𝓮)', // Your name
+  ownerNumber: '237659262653', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.4.0",
+  updateZipUrl: "https://github.com/queen-ai-bot-md/queen_ai_md-main.zip",
+};
+
+module.exports = settings;
